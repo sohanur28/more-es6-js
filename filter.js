bdf2c864713c8df5a604f6824599eaae793ae1ee
sohanur28 <sohanur28@gmail.com>
2025-06-24 19:23:15 +0600

    find

--- filter.js
+++ filter.js
@@ -4,5 +4,10 @@ const numbers = [1, 5, 6, 4, 15];
 const players = [75, 65, 67, 72, 55, 59];
 // const selected = players.filter( p => p > 70);
 // const selected = players.filter( p => p > 80);
-const selected = players.filter( p => p > 50);
+// const selected = players.filter( p => p > 50);
+const selected = players.filter(p => p % 2 === 1)
 console.log(selected);
+
+const friends = ['Tom', 'Jhon', 'Micheal', 'Oliver', 'Tim', 'Josna'];
+const oddFriends = friends.filter(friend => friend.length > 4);
+console.log(oddFriends);
